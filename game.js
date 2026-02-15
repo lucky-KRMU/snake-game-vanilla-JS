7d3a5a7062c777f4d9f4493c8ec2aa06819b87d7
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-15 06:36:37 +0000

    add final mobility logic of player

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 let board = document.getElementById("board"); // targeting the board
 let player = document.getElementById("player"); // targeting the player
 let gridSize = 8; // the size of the grid
-let fps = 500; // defined fps
+let fps = 300; // defined fps
 
-let movement = true;
+// let movement = true;
 
 let direction;
 
@@ -15,16 +15,20 @@ let playerPos ={ // storing the player position
 document.addEventListener("keydown", (e)=>{
     let key = e.key;
     if (key === "ArrowLeft" || key === "a" || key === "A"){
-        playerPos.x -= 1;
+        // playerPos.x -= 1;
+        direction = "-X";
         // movement = true;
     } else if (key === "ArrowRight" || key === "d" || key === "D"){
-        playerPos.x += 1;
+        // playerPos.x += 1;
+        direction = "+X";
         // movement = true;
     } else if (key === "ArrowDown" || key === "s" || key === "S"){
-        playerPos.y += 1;
+        // playerPos.y += 1;
+        direction = "+Y";
         // movement = false;
     } else if (key === "ArrowUp" || key === "w" || key === "W"){
-        playerPos.y -= 1;
+        // playerPos.y -= 1;
+        direction = "-Y";
         // movement = false;
     }
     console.log(key, playerPos);
@@ -32,8 +36,20 @@ document.addEventListener("keydown", (e)=>{
 
 setInterval(()=>{
     
-    // Condition to check that the player does not go out of the baord and return to the opposite edge.
     
+    
+    // updating the position of the player
+    if (direction === "+X") {
+        playerPos.x += 1;
+    } else if (direction === "-X"){
+        playerPos.x -= 1;
+    } else if (direction === "+Y"){
+        playerPos.y += 1;
+    } else if (direction === "-Y"){
+        playerPos.y -= 1;
+    }
+    
+    // Condition to check that the player does not go out of the baord and return to the opposite edge.
     if (playerPos.x < 1){
         playerPos.x = gridSize;
     } else if (playerPos.x > gridSize){
@@ -45,9 +61,11 @@ setInterval(()=>{
     } else if (playerPos.y < 1){
         playerPos.y = gridSize;
     }
-    
+
+
+
+    // Updating the position of the player
     player.style.gridColumn = playerPos.x;
     player.style.gridRow = playerPos.y;
-    playerPos.y+=1;
     
 }, fps);
