cb9f7693cab3ed644d4a3cb168ec61e307d92485
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-15 06:20:45 +0000

    update player move logic

--- game.js
+++ game.js
@@ -1,45 +1,53 @@
 let board = document.getElementById("board"); // targeting the board
 let player = document.getElementById("player"); // targeting the player
+let gridSize = 8; // the size of the grid
+let fps = 500; // defined fps
 
+let movement = true;
+
+let direction;
 
 let playerPos ={ // storing the player position
-    x: 1,
-    y: 1,
+    x: 5,
+    y: 5,
 }
 
 document.addEventListener("keydown", (e)=>{
     let key = e.key;
     if (key === "ArrowLeft" || key === "a" || key === "A"){
-        if (playerPos.x < 2){
-            playerPos.x = 9;
-        }
         playerPos.x -= 1;
-        player.style.gridColumn = playerPos.x;
+        // movement = true;
     } else if (key === "ArrowRight" || key === "d" || key === "D"){
-        if (playerPos.x > 7){
-            playerPos.x = 0;
-        }
         playerPos.x += 1;
-        player.style.gridColumn = playerPos.x;
+        // movement = true;
     } else if (key === "ArrowDown" || key === "s" || key === "S"){
-        if (playerPos.y > 7){
-            playerPos.y = 0;
-        }
         playerPos.y += 1;
-        player.style.gridRow = playerPos.y;
+        // movement = false;
     } else if (key === "ArrowUp" || key === "w" || key === "W"){
-        if (playerPos.y < 2){
-            playerPos.y = 9;
-        }
         playerPos.y -= 1;
-        player.style.gridRow = playerPos.y;
+        // movement = false;
     }
     console.log(key, playerPos);
 })
 
-// setInterval(()=>{
-//     player.style.gridColumn = playerPos.x;
-//     player.style.gridRow = playerPos.y;
-
-//     playerPos.x += 1;
-// }, 1000)
+setInterval(()=>{
+    
+    // Condition to check that the player does not go out of the baord and return to the opposite edge.
+    
+    if (playerPos.x < 1){
+        playerPos.x = gridSize;
+    } else if (playerPos.x > gridSize){
+        playerPos.x = 0;
+    }
+    
+    if (playerPos.y > gridSize){
+        playerPos.y = 0;
+    } else if (playerPos.y < 1){
+        playerPos.y = gridSize;
+    }
+    
+    player.style.gridColumn = playerPos.x;
+    player.style.gridRow = playerPos.y;
+    playerPos.y+=1;
+    
+}, fps);
