005a1f59f39a0dcf438a3d775fe456f1cbe099a9
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-15 10:39:18 +0000

    use obj snakeTrail

--- game.js
+++ game.js
@@ -2,17 +2,14 @@ let board = document.getElementById("board"); // targeting the board
 let player = document.getElementById("player"); // targeting the player
 let gridSize = 8; // the size of the grid
 let fps = 300; // defined fps
+let foodArray = []; // defining the food array
+let food = spawnFood(gridSize, foodArray); // spawing the initial food.
 
-let foodArray = [];
+let snakeTail = [];
+let snakeTrail = [];
 
-let initialFood = document.createElement('div');    // creating the initial food
-initialFood.className = 'food';
-initialFood.style.gridColumn = initialFood.style.gridRow = 4;
-// let food = board.appendChild(initialFood); // defining the food variable
-let food = spawnFood(gridSize, foodArray);
+let firstByte = true;
 
-
-// let movement = true;
 
 let direction; // defining the direction variable
 
@@ -21,6 +18,10 @@ let playerPos ={ // storing the player position
     y: 5,
 }
 
+let counter = 0;
+
+
+// function to change the direction of movement
 document.addEventListener("keydown", (e)=>{
     let key = e.key;
     if (key === "ArrowLeft" || key === "a" || key === "A"){
@@ -40,9 +41,12 @@ document.addEventListener("keydown", (e)=>{
         direction = "-Y";
         // movement = false;
     }
-    console.log(key, playerPos);
+    // console.log(key, playerPos);
 });
 
+
+
+// function to spawn food
 function spawnFood (gridSize){
     let foodX = Math.ceil( Math.random() * gridSize );
     let foodY = Math.ceil( Math.random() * gridSize );
@@ -58,16 +62,70 @@ function spawnFood (gridSize){
 
 }
 
+
+// function to handle the food consumption event
 function foodConsumed ( food, playerHead ) {
     if (food.style.gridColumn === playerHead.style.gridColumn && food.style.gridRow === playerHead.style.gridRow) {
         if (board.contains(food)){
-            board.removeChild(food);
-            console.log("food consumed!")
+
+            board.removeChild(food);    // removing the food.
+
+
+            let tail = document.createElement('div');
+            tail.className = 'player';
+            snakeTail.push(tail);
+            board.appendChild(tail);
+
+            let trail = {
+                x:0,
+                y:0
+            };
+
+            if (firstByte){
+
+                if (direction === "+X") {
+                    trail.x = playerPos.x - 1;
+                    trail.y = playerPos.y;
+                } else if (direction === "-X"){
+                    trail.x = playerPos.x + 1;   
+                    trail.y = playerPos.y;     
+                } else if (direction === "+Y"){
+                    trail.x = playerPos.x;
+                    trail.y = playerPos.y - 1;
+                } else if (direction === "-Y"){
+                    trail.x = playerPos.x;
+                    trail.y = playerPos.y + 1;
+                }
+                
+                firstByte = !firstByte;
+            } else {
+                
+                if (direction === "+X") {
+                    trail.x = snakeTrail[snakeTrail.length-1]["x"] - 1;
+                    trail.y = snakeTrail[snakeTrail.length-1]["y"];
+                } else if (direction === "-X"){
+                    trail.x = snakeTrail[snakeTrail.length-1]["x"] + 1;   
+                    trail.y = snakeTrail[snakeTrail.length-1]["y"];
+                } else if (direction === "+Y"){
+                    trail.x = snakeTrail[snakeTrail.length-1]["x"];
+                    trail.y = snakeTrail[snakeTrail.length-1]["y"] - 1;
+                } else if (direction === "-Y"){
+                    trail.x = snakeTrail[snakeTrail.length-1]["x"];
+                    trail.y = snakeTrail[snakeTrail.length-1]["y"] + 1;
+                } 
+            }
+
+
+
+            console.log(trail)
+            snakeTrail.push(trail);
+            console.log(snakeTrail);
         }
+        
     }
 }
 
-// spawnFood(gridSize);
+
 
 // interval to spawn food
 setInterval(() => {
@@ -76,23 +134,32 @@ setInterval(() => {
 
 
 
+// actual game loop
 setInterval(()=>{
-    
+
+
     // iterating over the food array to check for each consumption
     foodArray.forEach((food)=>{
         foodConsumed(food, player)
     })
     
+
+
+
+
     // updating the position of the player
     if (direction === "+X") {
         playerPos.x += 1;
     } else if (direction === "-X"){
-        playerPos.x -= 1;
+        playerPos.x -= 1;        
     } else if (direction === "+Y"){
         playerPos.y += 1;
     } else if (direction === "-Y"){
         playerPos.y -= 1;
     }
+
+
+
     
     // Condition to check that the player does not go out of the baord and return to the opposite edge.
     if (playerPos.x < 1){
@@ -109,8 +176,51 @@ setInterval(()=>{
 
 
 
+    snakeTrail.map( trail =>{
+        if (trail.x < 1){
+            trail.x = gridSize;
+        } else if (trail.x > gridSize){
+            trail.x = 0;
+        }
+
+        if (trail.y > gridSize){
+            trail.y = 0;
+        } else if (trail.y < 1){
+            trail.y = gridSize;
+        }
+        
+        if (direction === "+X") {
+            trail.x += 1;
+            // trail.y = trail.y;
+        } else if (direction === "-X"){
+            trail.x -= 1;   
+            // trail.y = trail.y;     
+        } else if (direction === "+Y"){
+            // trail.x = trail.x;
+            trail.y += 1;
+        } else if (direction === "-Y"){
+            // trail.x = trail.x;
+            trail.y -= 1;
+        }
+
+        
+        return trail;
+    })
+
+
+    // Logic for appending the tail of the snake
+    snakeTail.forEach((tailNode, index)=>{
+        tailNode.style.gridColumn = snakeTrail[index]["x"];
+        tailNode.style.gridRow = snakeTrail[index]["y"];
+
+    });
+
+
+
+
     // Updating the position of the player
     player.style.gridColumn = playerPos.x;
     player.style.gridRow = playerPos.y;
+
     
 }, fps);
